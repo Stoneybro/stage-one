702ce64c7234a25e57c88fa8dcc99765a04511df
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 16:23:56 +0100

    fix:fixed path bug

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -9,7 +9,7 @@ function Profile() {
     })
     return (
     <div className="profile">
-       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="/src/assets/profilehover.png" alt="" id="profile__img" />:<img src="/src/assets/profileimg.png" alt="" id="profile__img" />}</div>
+       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="src/assets/profilehover.png" alt="" id="profile__img" />:<img src="src/assets/profileimg.png" alt="" id="profile__img" />}</div>
         <div id="twitter">Stoneybro</div>
         <div id="slack">Zion</div>
     </div>
