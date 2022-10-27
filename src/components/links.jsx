c6d740075bf2b601f22a5b92708d515b4b1b4f12
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 15:16:10 +0100

    build:added links

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -4,12 +4,12 @@ function Links() {
 
     return (
    <div className="links">
-    <a href="" className="link">Twitter Link</a>
-    <a href="" className="link">Zuri Team</a>
-    <a href="" className="link">Zuri Books</a>
-    <a href="" className="link">Python Books</a>
-    <a href="" className="link">Background Check for Coders</a>
-    <a href="" className="link">Design Books</a>
+    <a href="https://twitter.com/Stoneybr0" className="link">Twitter Link</a>
+    <a href="https://training.zuri.team/" className="link">Zuri Team</a>
+    <a href="https://books.zuri.team/" className="link">Zuri Books</a>
+    <a href="https://books.zuri.team/python-for-beginners?ref_id=<zion>" className="link">Python Books</a>
+    <a href="https://background.zuri.team" className="link">Background Check for Coders</a>
+    <a href="https://books.zuri.team/design-rules" className="link">Design Books</a>
    </div>
      )
   }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,14 +1,18 @@
+import { useEffect } from "react"
+import { useState } from "react"
+
 
 function Profile() {
-
-
+    const [hover,sethover]=useState(false)
+    useEffect(()=>{
+        
+    })
     return (
     <div className="profile">
-        <img src="./src/assets/profileimg.png" alt="" id="profile__img" />
+       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="./src/assets/profilehover.png" alt="" id="profile__img" />:<img src="./src/assets/profileimg.png" alt="" id="profile__img" />}</div>
         <div id="twitter">Stoneybro</div>
         <div id="slack">Zion</div>
     </div>
      )
   }
-  
   export default Profile
