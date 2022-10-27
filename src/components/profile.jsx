6c0601bd4a3d3e0438084aada164cad950103fab
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 22:24:02 +0100

    build:added share button

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -2,12 +2,17 @@
 import { useState } from "react"
 import pic from "../assets/profilehover.png"
 import choosepic from "../assets/profileimg.png"
+import share from '../assets/share.png'
+import mobileshare from '../assets/mobileshare.png'
 
 function Profile() {
     const [hover,sethover]=useState(false)
     return (
     <div className="profile">
-       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src={pic} alt="profile picture" id="profile__img" />:<img src={choosepic} alt="choose profile picture" id="profile__img" />}</div>
+       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src={pic} alt="profile picture" id="profile__img" />:<img src={choosepic} alt="choose profile picture" id="profile__img" />}
+       </div>
+        <img className="share share-desktop" src={share} alt="" />
+        <img src={mobileshare} className="share share-mobile" alt="" />
         <div id="twitter">Stoneybro</div>
         <div id="slack">Zion</div>
     </div>
