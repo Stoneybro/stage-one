b8568bf7bab6ef0755fe92d45e22375fd723544e
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 16:32:38 +0100

    fix:fixed bug path

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,16 @@
-
+import slack from '../assets/slack.png'
 function Footer() {
 
 
     return (
    <footer>
     <div className="socials">
-        <img src="/assets/slack.png" alt="slack icon" /><img src="/assets/github.png" alt="github icon" />
+        <img src={slack} alt="slack icon" /><img src="src/assets/github.png" alt="github icon" />
     </div>
     <div className="sponsors">
-        <img src="/assets/zuri.png" alt="" />
+        <img src="src/assets/zuri.png" alt="" />
         <div>HNG Internship 9 Frontend Task</div>
-        <img src="/assets/I4G.png" alt="" />
+        <img src="/src/assets/I4G.png" alt="" />
     </div>
    </footer>
      )
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -1,15 +1,12 @@
-import { useEffect } from "react"
+
 import { useState } from "react"
 
 
 function Profile() {
     const [hover,sethover]=useState(false)
-    useEffect(()=>{
-        
-    })
     return (
     <div className="profile">
-       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="/assets/profilehover.png" alt="" id="profile__img" />:<img src="src/assets/profileimg.png" alt="" id="profile__img" />}</div>
+       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="src/assets/profilehover.png" alt="" id="profile__img" />:<img src="/src/assets/profileimg.png" alt="" id="profile__img" />}</div>
         <div id="twitter">Stoneybro</div>
         <div id="slack">Zion</div>
     </div>
