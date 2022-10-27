55060473b21712db73f41999e626109ea2ec8b09
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 16:15:27 +0100

    fix:fixed path bug

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,12 +5,12 @@ function Footer() {
     return (
    <footer>
     <div className="socials">
-        <img src="./src/assets/slack.png" alt="slack icon" /><img src="./src/assets/github.png" alt="github icon" />
+        <img src="../src/assets/slack.png" alt="slack icon" /><img src="../src/assets/github.png" alt="github icon" />
     </div>
     <div className="sponsors">
-        <img src="./src/assets/zuri.png" alt="" />
+        <img src="../src/assets/zuri.png" alt="" />
         <div>HNG Internship 9 Frontend Task</div>
-        <img src="./src/assets/I4G.png" alt="" />
+        <img src="../src/assets/I4G.png" alt="" />
     </div>
    </footer>
      )
