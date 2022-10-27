ce1f1736c503285f141426a606a5920b02058805
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 21:33:44 +0100

    fix:fixed bug path

--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -6,7 +6,7 @@ function Profile() {
     const [hover,sethover]=useState(false)
     return (
     <div className="profile">
-       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="src/assets/profilehover.png" alt="" id="profile__img" />:<img src="/src/assets/profileimg.png" alt="" id="profile__img" />}</div>
+       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src="../assets/profilehover.png" alt="" id="profile__img" />:<img src="/src/assets/profileimg.png" alt="" id="profile__img" />}</div>
         <div id="twitter">Stoneybro</div>
         <div id="slack">Zion</div>
     </div>
