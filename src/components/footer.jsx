af65f7760e3add3e17d40be490beee513de48dc9
zion livingstone <zionlivingstone@gmail.com>
2022-10-27 21:30:55 +0100

    test:styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,10 +5,10 @@ function Footer() {
     return (
    <footer>
     <div className="socials">
-        <img src={slack} alt="slack icon" /><img src="src/assets/github.png" alt="github icon" />
+        <img src='../assets/slack.png' alt="slack icon" /><img src="src/assets/github.png" alt="github icon" />
     </div>
     <div className="sponsors">
-        <img src="src/assets/zuri.png" alt="" />
+        <img src="/src/assets/zuri.png" alt="" />
         <div>HNG Internship 9 Frontend Task</div>
         <img src="/src/assets/I4G.png" alt="" />
     </div>
