69190f2acf894849e8c18eda50e607f8ed96cc86
zion livingstone <zionlivingstone@gmail.com>
2022-10-28 13:59:06 +0100

    fix:styles

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -4,12 +4,12 @@ function Links() {
 
     return (
    <div className="links">
-    <a href="https://twitter.com/Stoneybr0" className="link">Twitter Link</a>
+    <a href="https://twitter.com/Stoneybr0" className="link" id="btn__zuri">Twitter Link</a>
     <a href="https://training.zuri.team/" className="link">Zuri Team</a>
-    <a href="https://books.zuri.team/" className="link">Zuri Books</a>
-    <a href="https://books.zuri.team/python-for-beginners?ref_id=<zion>" className="link">Python Books</a>
-    <a href="https://background.zuri.team" className="link">Background Check for Coders</a>
-    <a href="https://books.zuri.team/design-rules" className="link">Design Books</a>
+    <a href="https://books.zuri.team/" className="link" id="books">Zuri Books</a>
+    <a href="https://books.zuri.team/python-for-beginners?ref_id=<Zion>" className="link" id="book__python">Python Books</a>
+    <a href="https://background.zuri.team" className="link" id="pitch">Background Check for Coders</a>
+    <a href="https://books.zuri.team/design-rules" className="link" id="book__design">Design Books</a>
    </div>
      )
   }
--- src/components/profile.jsx
+++ src/components/profile.jsx
@@ -9,7 +9,8 @@ function Profile() {
     const [hover,sethover]=useState(false)
     return (
     <div className="profile">
-       <div onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src={pic} alt="profile picture" id="profile__img" />:<img src={choosepic} alt="choose profile picture" id="profile__img" />}
+        <div className="profile-wrapper"><div className="profile-img" onMouseOver={()=>sethover(true)} onMouseLeave={()=>sethover(false)}>{hover?<img src={pic} alt="profile picture" id="profile__img" />:<img src={choosepic} alt="choose profile picture" id="profile__img" />}
+</div>
        </div>
         <img className="share share-desktop" src={share} alt="" />
         <img src={mobileshare} className="share share-mobile" alt="" />
